4118366bc05417205e23abf2b297e2a6e1075585
NILTON SOUSA <niltonmiguel0@gmail.com>
2026-03-20 19:58:47 -0300

    api completa

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
@@ -10,17 +11,16 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     //criando o página inicial para retornar todos os usuarios
-    public funtion index(){
+    public function index(){
         //salvando todos os meus usuarios dentro da variavel users
         $users = User::all();
         //retornando um json com todos os usuarios e a resposta de ok
         return response()->json($users , Response::HTTP_OK);
-    };
-
-
+    }
 
   //criando a função para login
   //pegando os dados vindo do request ou formularios
+
     public function login(Request $request){
 
         //fazendo a validação dos campos
@@ -39,21 +39,32 @@ class UserController extends Controller
             'message' => 'Invalid credentials',
             ]
             ,Response::HTTP_UNAUTHORIZED);// retornando a resposta de não autorizado
-        };
+        }
+
+            //criando meu token e salvando na variavel token
+            $token = $user->createToken($request->email)->plainTextToken;
+            //retornando meu token
+            return response()->json([
+                'status' => 'Success',
+                'message' => 'login successful',
+                'token' => $token,
+            ], Response::HTTP_OK);
+        }
         
 
 
         
-    //fazendo o logout
-    public function logout(Request $request){
-        //pego o meu o token do meu usuario atual e deleto ele
-        $request->user()->currentAccessToken()->delete();
-        return response()->json([
-            'status' => 'success',
-            'message' => 'LogOut SuccessFul',
-        ],
-        Response::HTTP_OK);
-    };
+        //fazendo o logout
+
+        public function logout(Request $request){
+            //pego o meu o token do meu usuario atual e deleto ele
+            $request->user()->currentAccessToken()->delete();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'LogOut SuccessFul',
+            ],
+            Response::HTTP_OK);
+        }
 
 
         
@@ -77,10 +88,9 @@ class UserController extends Controller
         return response()->json([
             'status' => 'success',
             'message' => 'register successful',
-            'user' => $user;
+            'user' => $user,
         ],Response::HTTP_CREATED);
-    };
-
+    }
 
 
         
@@ -99,22 +109,19 @@ class UserController extends Controller
         }
         //se tudo ocorrer certo me retorna o usuario atual e a resposta ok
         return response()->json($user, Response::HTTP_OK);
-    };
-
-        
-
+    }
 
         
         //criando a função para atualizar o usuario atual
-        public function update(Resquest $request , $id){
+        public function update(Request $request , $id){
             //pegando meu usuario atual e salvando dentro da variavel user
             $user = User::find($id);
             //checando se o usuario não existe 
-            if(!user){
+            if(!$user){
                 //me retornando um json com erro 
                 return response()->json(['status'=> 'error',
                 'message' => 'user not found'], Response::HTTP_NOT_FOUND);
-            };
+            }
 
             //caso o usuario exista faço a validação dos campos
             $request->validate([
@@ -143,19 +150,6 @@ class UserController extends Controller
                 'user' => $user,
             ],Response::HTTP_OK);
         }
-
-
-        
-        
-        //criando o token da api para manter o usuario logado
-        $token = $user->createToken($request->email)->plainTextToken;
-        return response()->json([
-            'status' => 'ok',
-            'message' => 'login successful',
-            'token' => $token, //passando o token como resposta para salvar detnro do banco de dados
-        ],
-        Response::HTTP_OK); //retornando a resposta de status ok
-    }
 
 
 
